1bfce87a3283b839f06812728b6c222e2e1fb3dc
Donald Wu <yeukfei02@gmail.com>
2022-04-20 21:03:41 +0800

    fix: 🐛 add PrismaService in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { LocationModule } from './location/location.module';
 import { PostModule } from './post/post.module';
 import { TagModule } from './tag/tag.module';
 import { CommentModule } from './comment/comment.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [UserModule, LocationModule, PostModule, TagModule, CommentModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
